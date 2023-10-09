ab8142ec9b52960f9f22502ddc1b43a847bda84f
Leerling Oostpoort <129111356+leerling-oostpoort@users.noreply.github.com>
2023-10-09 23:49:02 +0200

    Initial version keeping distance

--- main.ts
+++ main.ts
@@ -1,1 +1,9 @@
-maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 45)
+basic.forever(function () {
+    if (maqueen.Ultrasonic(PingUnit.Centimeters) < 8) {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 20)
+    } else if (maqueen.Ultrasonic(PingUnit.Centimeters) < 12) {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 0)
+    } else {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 45)
+    }
+})
